8259985fa0a6589069ba05878ac96662c0caf702
boscotron <boscotron@users.noreply.github.com>
2019-03-28 14:24:46 -0600

    Initial commit

---
+++ functions/index.js
@@ -0,0 +1,60 @@
+/*
+La licencia MIT (MIT)
+
+Copyright (c) 2019 Concomsis S.A. de C.V.
+
+Por la presente se otorga el permiso, sin cargo, a cualquier persona que obtenga una copia de
+este software y los archivos de documentación asociados (el "Software"), para tratar en
+el Software sin restricciones, incluidos, entre otros, los derechos de
+usar, copiar, modificar, fusionar, publicar, distribuir, sublicenciar y / o vender copias de
+el Software, y para permitir que las personas a quienes se suministra el Software lo hagan,
+sujeto a las siguientes condiciones:
+
+El aviso de copyright anterior y este aviso de permiso se incluirán en todas las
+Copias o partes sustanciales del Software.
+
+EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O
+IMPLÍCITOS, INCLUIDOS, PERO NO LIMITADOS A LAS GARANTÍAS DE COMERCIABILIDAD, APTITUD
+PARA UN PROPÓSITO PARTICULAR Y NO INCUMPLIMIENTO. EN NINGÚN CASO LOS AUTORES O
+LOS TITULARES DEL DERECHO DE AUTOR SERÁN RESPONSABLES POR CUALQUIER RECLAMACIÓN, DAÑOS U OTRAS RESPONSABILIDADES, SI
+EN UNA ACCIÓN DE CONTRATO, CORTE O DE OTRA MANERA, DERIVADO DE, FUERA O EN
+CONEXIÓN CON EL SOFTWARE O EL USO U OTRAS REPARACIONES EN EL SOFTWARE.
+*/
+'use strict';
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const language = require('@google-cloud/language');
+const client = new language.LanguageServiceClient();
+const express = require('express');
+const app = express();
+
+const jmy = require('comsis_jmy');
+
+const acceso = {
+  servidor:"",
+  eid:"",
+  uid:"",
+  token:""
+};
+
+ jmy.token([],acceso).then(function(e){
+    console.log("token",e);  
+  });
+
+
+app.post('/ver', async (req, res) => {
+  const ver = req.body;
+  try {
+    console.log(ver);
+    res.status(201).json(ver);
+  } catch(error) {
+    console.log('Error detecting sentiment or saving message', error.message);
+    res.sendStatus(500);
+  }
+});
+
+// Expose the API as a function
+exports.api = functions.https.onRequest(app);
+
